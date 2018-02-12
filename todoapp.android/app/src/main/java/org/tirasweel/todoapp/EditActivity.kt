0a610d67646c70107cf15abae13c0212a46073ee
Katsuki Kobayashi <rare@tirasweel.org>
2018-02-12 13:28:47 +0900

    詳細画面と編集画面のアクティビティを追加(中身は空).

---
+++ todoapp.android/app/src/main/java/org/tirasweel/todoapp/EditActivity.kt
@@ -0,0 +1,17 @@
+package org.tirasweel.todoapp
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_edit.*
+
+class EditActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit)
+        setSupportActionBar(toolbar)
+    }
+
+}
